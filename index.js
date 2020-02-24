bd23188255cebcabb3727f9b496987169b4587eb
GuillermoFR <bengallme@gmail.com>
2020-02-24 13:51:38 -0600

    added a really basic shopping list

--- index.js
+++ index.js
@@ -1,34 +1,11 @@
-// const colors = [
-//   "red",
-//   "orange",
-//   "yellow",
-//   "green",
-//   "blue",
-//   "purple",
-//   "indigo",
-//   "violet"
-// ];
-const colors = [];
-const changeColor = function() {
-  const h1 = document.querySelector("h1");
-  h1.style.color = this.style.backgroundColor;
-};
-function randomColor() {
-  let r = Math.floor(Math.random() * 256);
-  let g = Math.floor(Math.random() * 256);
-  let b = Math.floor(Math.random() * 256);
-  return { r, g, b };
-}
-for (let i = 0; i <= 8; i++) {
-  let { r, g, b } = randomColor();
-  colors.push(`rgb(${r},${g},${b})`);
-}
+const addItemInput = document.querySelector("#addItem");
+const itemsUl = document.querySelector("#items");
 
-const container = document.querySelector("#boxes");
-for (let color of colors) {
-  const box = document.createElement("div");
-  box.style.backgroundColor = color;
-  box.classList.add("box");
-  container.append(box);
-  box.addEventListener("click", changeColor);
-}
+addItemInput.addEventListener("keypress", function(e) {
+  if (e.key === "Enter") {
+    const newLi = document.createElement("li");
+    newLi.innerText = this.value;
+    itemsUl.append(newLi);
+    this.value = "";
+  }
+});
