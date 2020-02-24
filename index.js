3c178ddb12d51f258ba23fd472d3e8b431ed0117
GuillermoFR <bengallme@gmail.com>
2020-02-24 12:38:33 -0600

    added annoying button

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+const btn = document.querySelector("button");
+btn.addEventListener("mouseover", function() {
+  let { width, height } = randomLocation();
+  btn.style.left = `${width}px`;
+  btn.style.top = `${height}px`;
+});
 
+btn.addEventListener("click", function() {
+  btn.innerText = "You got me, cowboy";
+  document.body.style.backgroundColor = "#9147FF";
+});
+
+function randomLocation() {
+  let randomX = Math.floor(Math.random() * window.innerWidth);
+  let randomY = Math.floor(Math.random() * window.innerHeight);
+
+  return { width: randomX, height: randomY };
+}
