cdf7bbdd4e95186901eebed0d2d78008058799b7
meme <bengallme@gmail.com>
2020-03-09 13:02:45 -0600

    added a test with axios and SWAPI api

--- index.js
+++ index.js
@@ -1,11 +1,67 @@
-const addItemInput = document.querySelector("#addItem");
-const itemsUl = document.querySelector("#items");
+axios
+  .get("https://swapi.co/api/planets/")
+  .then(({ data }) => {
+    for (let planet of data.results) {
+      console.log(planet.name);
+    }
+    return axios.get(data.next);
+  })
+  .then(({ data }) => {
+    for (let planet of data.results) {
+      console.log(planet.name);
+    }
+    return axios.get(data.next);
+  })
+  .catch(e => {
+    console.log("something went wrong");
+    console.log(e);
+  });
 
-addItemInput.addEventListener("keypress", function(e) {
-  if (e.key === "Enter") {
-    const newLi = document.createElement("li");
-    newLi.innerText = this.value;
-    itemsUl.append(newLi);
-    this.value = "";
-  }
-});
+// const firstReq = new XMLHttpRequest();
+// firstReq.addEventListener("load", () => {
+//   console.log("first request worked!");
+//   const data = JSON.parse(firstReq.responseText);
+//   const filmUrl = data.results[0].films[0];
+//   const filmReq = new XMLHttpRequest();
+//   filmReq.addEventListener("load", () => {
+//     console.log("Second request worked");
+//     const filmData = JSON.parse(filmReq.responseText);
+//     console.log(filmData.title);
+//   });
+//   filmReq.addEventListener("error", e => {
+//     console.log("error!", e);
+//   });
+//   filmReq.open("GET", filmUrl);
+//   filmReq.send();
+// });
+// firstReq.addEventListener("error", () => {
+//   console.log("it failed!");
+// });
+// firstReq.open("GET", "https://swapi.co/api/planets/");
+// firstReq.send();
+// console.log("request sent!");
+// const printData = data => {
+//   for (let planet of data.results) {
+//     console.log(planet.name);
+//   }
+//   const nextPageURL = data.next;
+//   return fetch(nextPageURL);
+// };
+
+// const prom = fetch("https://swapi.co/api/planets/")
+//   .then(response => {
+//     return response.json();
+//   })
+//   .then(printData)
+//   .then(response => {
+//     return response.json();
+//   })
+//   .then(printData)
+//   .then(response => {
+//     return response.json();
+//   })
+//   .then(printData)
+//   .catch(err => {
+//     console.log("something went wrong");
+//     console.log(err);
+//   });
