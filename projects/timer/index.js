86f083e044bb72e6727d4bd5ebe2fe9dc9be7d4f
GuillermoFR <bengallme@gmail.com>
2020-03-31 00:46:24 -0600

    timer now works

---
+++ projects/timer/index.js
@@ -0,0 +1,38 @@
+class Timer {
+  constructor(durationInput, startButton, pauseButton) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+
+    this.startButton.addEventListener("click", this.start);
+    this.pauseButton.addEventListener("click", this.pause);
+  }
+  start = () => {
+    this.tick();
+    this.interval = setInterval(this.tick, 1000);
+  };
+  tick = () => {
+    if (this.timeRemaining <= 0) {
+      this.pause();
+    } else {
+      this.timeRemaining = this.timeRemaining - 1;
+    }
+  };
+  pause = () => {
+    clearInterval(this.interval);
+    console.log("Timer has stopped");
+  };
+
+  get timeRemaining() {
+    return parseFloat(this.durationInput.value);
+  }
+  set timeRemaining(time) {
+    this.durationInput.value = time;
+  }
+}
+
+const startButton = document.querySelector("#start");
+const pauseButton = document.querySelector("#pause");
+const durationInput = document.querySelector("#duration");
+
+const timer = new Timer(durationInput, startButton, pauseButton);
